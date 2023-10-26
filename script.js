a9940bc21ba32279d652278792d6954abf4c0623
AndrePacolla <andreluizzpacolla@outlook.com>
2023-10-26 15:49:04 -0300

    udpate style  div result

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-document.querySelector('.busca').addEventListener('submit',(event)=>{
-    event.preventDefault();
 
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault();
     let input = document.querySelector('#searchInput').value;
 
     if(input !== ''){
-        showWarning();
+
+        document.querySelector('.img').src ='assests/loading.gif'
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=2bf2545b6be242cedba39401909dd0d1&units=metric&lang=pt_br`
+        
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.code === 200){
+
+        }else{
+            showWarning('Localização não encontrada !')
+        }
 
     }else{
 
     }
 
 });
-function showWarning(){
-    document.querySelector('.img').src = 'assests/loading.gif';
-    
-
-
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg;
 
 }
