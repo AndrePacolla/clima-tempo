a9d5c3bfc8c894f0352d0ad8b6ac386dffc76038
AndrePacolla <andreluizzpacolla@outlook.com>
2023-10-26 13:01:14 -0300

    adc de git com js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 document.querySelector('.busca').addEventListener('submit',(event)=>{
     event.preventDefault();
 
+    let input = document.querySelector('#searchInput').value;
 
+    if(input !== ''){
+        showWarning();
+
+    }else{
+
+    }
 
 });
+function showWarning(){
+    document.querySelector('.img').src = 'assests/loading.gif';
+    
+
+
+
+}
